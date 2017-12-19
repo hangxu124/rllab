f1670928c710a241fc1d85c8fcc7c4174e076a32
Hang Xu <31737143+hangxu124@users.noreply.github.com>
2017-12-19 15:58:06 +0100

    Add files via upload

--- armenv.py
+++ armenv.py
@@ -1,72 +1,21 @@
 import numpy as np
 from rllab.envs.box2d.parser import find_body
-import re
 from rllab.core.serializable import Serializable
 from rllab.envs.box2d.box2d_env import Box2DEnv
 from rllab.misc import autoargs
 from rllab.misc.overrides import overrides
+import time
 
-'''
 class ArmEnv(Box2DEnv, Serializable):
     @autoargs.inherit(Box2DEnv.__init__)
     def __init__(self, *args, **kwargs):
-        # make sure mdp-level step is 100ms long
         kwargs["frame_skip"] = kwargs.get("frame_skip", 2)
         if kwargs.get("template_args", {}).get("noise", False):
-            self.link_len = (np.random.rand() - 0.5) + 1
+            self.link_len = (np.random.rand()-0.5) + 1
         else:
             self.link_len = 1
         kwargs["template_args"] = kwargs.get("template_args", {})
         kwargs["template_args"]["link_len"] = self.link_len
-        super(ArmEnv, self).__init__(
-            self.model_path("arm_env.xml.mako"),
-            *args, **kwargs
-        )
-        self.arm1 = find_body(self.world, "arm1")
-        self.arm2 = find_body(self.world, "arm2")
-        Serializable.__init__(self, *args, **kwargs)
-
-
-
-    def printinfo(self):
-        #print (self.get_current_obs())
-        #print (self.world.bodies)
-        a=str(self.arm2.fixtures)
-        match1=re.search(r"vertices:",a).span()
-        m1=match1[1]
-        ver=a[m1+2:]
-        match2=re.search(r"]" , ver).span()
-        m2=match2[0]
-        ver=ver[:m2]
-        ver1=ver.replace("(","")
-        ver2=ver1.replace(")","")
-        ma=ver2.split(", ")
-
-        x=(float(ma[2])+float(ma[4]))*0.5+self.arm2.position[0]
-        y=(float(ma[3])+float(ma[5]))*0.5+self.arm2.position[1]
-        print (x,y)
-
-
-        f=open("info.txt",'w')
-        f.write(str(self.arm2))
-        #print(self.track.position)
-
-'''
-
-
-
-
-import numpy as np
-from rllab.misc.mako_utils import compute_rect_vertices
-from rllab.envs.box2d.parser import find_body
-from rllab.core.serializable import Serializable
-from rllab.envs.box2d.box2d_env import Box2DEnv
-from rllab.misc import autoargs
-from rllab.misc.overrides import overrides
-
-class ArmEnv(Box2DEnv, Serializable):
-    @autoargs.inherit(Box2DEnv.__init__)
-    def __init__(self, *args, **kwargs):
 
         #self.dt = .1    # refresh rate
         #self.on_goal = 0
@@ -87,8 +36,8 @@ class ArmEnv(Box2DEnv, Serializable):
         self._set_state(self.initial_state)
         self._invalidate_state_caches()
         bounds = np.array([
-            [-np.pi/2,np.pi/2],
-            [-np.pi/2,np.pi/2]
+            [-np.pi,np.pi],
+            [-np.pi,np.pi]
         ])
         low, high = bounds
         aarm1,aarm2 = np.random.uniform(low, high)
@@ -100,7 +49,7 @@ class ArmEnv(Box2DEnv, Serializable):
     @overrides
     def compute_reward(self, action):
         yield
-        self.vertices[0] ,self.vertices[1]=self.calculate_pos()
+        self.vertices[0] ,self.vertices[1]=self.get_tip_pos()
         #dist1 = [(0.1 - self.arm1.vertices[0]), (-0.5 - self.arm1.vertices[1])]
         dist2 = [(1 - self.vertices[0]), (0.6 - self.vertices[1])]
         yield -np.sqrt(dist2[0]**2+dist2[1]**2)
@@ -109,27 +58,19 @@ class ArmEnv(Box2DEnv, Serializable):
 
     @overrides
     def is_current_done(self):
-        a=(1 - 0.05 < self.vertices[0] < 1 + 0.05) and \
-        (0.6 - 0.05 < self.vertices[1] < 0.6 + 0.05)
+        a=((1 - 0.05 < self.vertices[0] < 1 + 0.05) and \
+        (0.6 - 0.05 < self.vertices[1] < 0.6 + 0.05))
         if a: print(a)
         return a
 
-    def calculate_pos(self):
-        a = str(self.arm2.fixtures)
-        match1 = re.search(r"vertices:", a).span()
-        m1 = match1[1]
-        ver = a[m1 + 2:]
-        match2 = re.search(r"]", ver).span()
-        m2 = match2[0]
-        ver = ver[:m2]
-        ver1 = ver.replace("(", "")
-        ver2 = ver1.replace(")", "")
-        ma = ver2.split(", ")
-
-        x = (float(ma[2]) + float(ma[4])) * 0.5 + self.arm2.position[0]
-        y = (float(ma[3]) + float(ma[5])) * 0.5 + self.arm2.position[1]
-        return x,y
-
+    def get_tip_pos(self):
+        cur_center_pos = self.arm2.position
+        cur_angle = self.arm2.angle
+        cur_pos = (
+            cur_center_pos[0] - self.link_len*np.sin(cur_angle),
+            cur_center_pos[1] + self.link_len*np.cos(cur_angle)
+        )
+        return cur_pos
 
 
 
@@ -138,12 +79,21 @@ if __name__=="__main__":
     ar=ArmEnv()
     #ca.printinfo()
     #print (type(ar.extra_data.states))
-    print (ar.world.bodies)
-    n=1
-    for state in ar.extra_data.states:
-        #print (n)
-        #print (find_body(ar.world, state.body))
-        #print (body)
-        n+=1
-    #while True:
-    #   ar.render()
+    a=[0,0]
+    b=[0,0]
+    ar.render()
+    time.sleep(2)
+    while True:
+
+        #print ("vertices:",ar.vertices)
+        ar.step(np.array([30]))
+        #print("arm2:", ar.arm2.position)
+        #print ("tip:",ar.get_tip_pos())
+        ar.render()
+        a[0],a[1]=ar.get_tip_pos()
+        b[0],b[1]=ar.arm2.position
+        print (ar.is_current_done())
+
+        #print (((abs(a[0]-b[0]))**2+ (abs(a[1]-b[1])**2))**0.5)
+
+        time.sleep(0.2)
